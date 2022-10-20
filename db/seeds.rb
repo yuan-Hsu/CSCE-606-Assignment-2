e236c29c100ee943c7077adae9d4b32e541d960d
EC2 Default User <ec2-user@ip-172-31-8-237.us-east-2.compute.internal>
2022-10-20 21:59:55 +0000

    add my favorite movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,11 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Spider-Man: No Way Home', :rating => 'PG-13',
+    :release_date => '13-Dec-2021'},
+  {:title => 'Spider-Man: Far From Home', :rating => 'PG-13',
+    :release_date => '2-Jul-2019'}
 ]
 
 more_movies.each do |movie|
